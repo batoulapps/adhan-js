7a87b1b5011f1d90f96337b2e6ecd02cc827dbe3
Basem Emara <contact@basememara.com>
2017-06-17 08:25:49 -0400

    Added multiple targets and associated unit tests for various platforms

--- Swift/Sources/Adhan.h
+++ Swift/Sources/Adhan.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Batoul Apps. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Adhan.
 FOUNDATION_EXPORT double AdhanVersionNumber;
