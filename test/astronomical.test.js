c517d4565f950a499303de44b4dd067427fcf642
Ameir Al-Zoubi <aalzoubi@switchfour.io>
2018-09-17 11:23:19 -0400

    fix travis testing

--- test/astronomical.test.js
+++ test/astronomical.test.js
@@ -105,145 +105,135 @@ describe("Astronomical Equations", function() {
 	    var rise = adhan.Astronomical.correctedHourAngle(m0, -0.5667, coordinates, false, Theta, Alpha2, Alpha1, Alpha3, Delta2, Delta1, Delta3) / 24;
 	    expect(rise).to.be.closeTo(0.51766, 0.00001);
 	});
+
+	it("calculates Solar Time values", function() {
+	    // Comparison values generated from http://aa.usno.navy.mil/rstt/onedaytable?form=1&ID=AA&year=2015&month=7&day=12&state=NC&place=raleigh
+	    var coordinates = new adhan.Coordinates(35 + 47/60, -78 - 39/60);
+	    var solar = new adhan.SolarTime(new Date(2015, 6, 12), coordinates);
+
+	    var transit = solar.transit;
+	    var sunrise = solar.sunrise;
+	    var sunset = solar.sunset;
+	    var twilightStart = solar.hourAngle(-6, false);
+	    var twilightEnd = solar.hourAngle(-6, true);
+	    var invalid = solar.hourAngle(-36, true);
+	    expect(timeString(twilightStart)).to.equal("9:38");
+	    expect(timeString(sunrise)).to.equal("10:08");
+	    expect(timeString(transit)).to.equal("17:20");
+	    expect(timeString(sunset)).to.equal("24:32");
+	    expect(timeString(twilightEnd)).to.equal("25:02");
+	    expect(timeString(invalid)).to.equal("");
+	});
+
+	it("verifies Right Ascension Edge Case", function() {
+	    var coordinates = new adhan.Coordinates(35 + 47/60, -78 - 39/60);
+	    var solar = [];
+	    for (var i = 0; i <= 365; i++) {
+	        solar.push(new adhan.SolarTime(new Date(2016, 0, i), coordinates));
+	    }
+
+	    for (var i = 1; i < solar.length; i++) {
+	        var time = solar[i];
+	        var previousTime = solar[i-1];
+	        expect(Math.abs(time.transit - previousTime.transit) < 1/60).to.be.true;
+	        expect(Math.abs(time.sunrise - previousTime.sunrise) < 2/60).to.be.true;
+	        expect(Math.abs(time.sunset - previousTime.sunset) < 2/60).to.be.true;
+	    }
+	});
+
+	it("verifies the correct calendar date is being used for calculations", function() {
+		// generated from http://aa.usno.navy.mil/data/docs/RS_OneYear.php for KUKUIHAELE, HAWAII
+	    var coordinates = new adhan.Coordinates(20 + 7/60, -155 - 34/60);
+	    var day1solar = new adhan.SolarTime(new Date(2015, 3, 2), coordinates);
+	    var day2solar = new adhan.SolarTime(new Date(2015, 3, 3), coordinates);
+
+	    var day1 = day1solar.sunrise;
+	    var day2 = day2solar.sunrise;
+
+	    expect(timeString(day1)).to.equal("16:15")
+	    expect(timeString(day2)).to.equal("16:14")
+	});
+
+	it("interpolates a value given previous and next values along with an interpolation factor", function() {
+		var interpolatedValue = adhan.Astronomical.interpolate(0.877366, 0.884226, 0.870531, 4.35/24)
+		expect(interpolatedValue).to.be.closeTo(0.876125, 0.000001);
+
+	    var i1 = adhan.Astronomical.interpolate(1, -1, 3, 0.6);
+	    expect(i1).to.be.closeTo(2.2, 0.000001);
+
+	    var i2 = adhan.Astronomical.interpolateAngles(1, -1, 3, 0.6);
+	    expect(i2).to.be.closeTo(2.2, 0.000001);
+
+	    var i3 = adhan.Astronomical.interpolateAngles(1, 359, 3, 0.6);
+	    expect(i3).to.be.closeTo(2.2, 0.000001);
+	});
+
+	it("calculates the Julian day for a given Gregorian date", function() {
+		// Comparison values generated from http://aa.usno.navy.mil/data/docs/JulianDate.php
+		expect(adhan.Astronomical.julianDay(2010, 1, 2)).to.equal(2455198.500000);
+		expect(adhan.Astronomical.julianDay(2011, 2, 4)).to.equal(2455596.500000);
+		expect(adhan.Astronomical.julianDay(2012, 3, 6)).to.equal(2455992.500000);
+		expect(adhan.Astronomical.julianDay(2013, 4, 8)).to.equal(2456390.500000);
+		expect(adhan.Astronomical.julianDay(2014, 5, 10)).to.equal(2456787.500000);
+		expect(adhan.Astronomical.julianDay(2015, 6, 12)).to.equal(2457185.500000);
+		expect(adhan.Astronomical.julianDay(2016, 7, 14)).to.equal(2457583.500000);
+		expect(adhan.Astronomical.julianDay(2017, 8, 16)).to.equal(2457981.500000);
+		expect(adhan.Astronomical.julianDay(2018, 9, 18)).to.equal(2458379.500000);
+		expect(adhan.Astronomical.julianDay(2019, 10, 20)).to.equal(2458776.500000);
+		expect(adhan.Astronomical.julianDay(2020, 11, 22)).to.equal(2459175.500000);
+		expect(adhan.Astronomical.julianDay(2021, 12, 24)).to.equal(2459572.500000);
+
+		var jdVal = 2457215.67708333;
+		expect(adhan.Astronomical.julianDay(2015, 7, 12, 4.25)).to.be.closeTo(jdVal, 0.000001);
+
+	    var date = new Date(2015, 6, 12, 4, 15);
+	    expect(julianDate(date), jdVal, 0.000001);
+
+		expect(adhan.Astronomical.julianDay(2015, 7, 12, 8.0)).to.be.closeTo(2457215.833333, 0.000001);
+		expect(adhan.Astronomical.julianDay(1992, 10, 13, 0.0)).to.be.closeTo(2448908.5, 0.000001);
+
+		var j1 = adhan.Astronomical.julianDay(2010, 1, 3);
+		var j2 = adhan.Astronomical.julianDay(2010, 1, 1, 48);
+		expect(j1).to.equal(j2);
+	});
+
+	it("determines if a year is a leap year", function() {
+		expect(adhan.Astronomical.isLeapYear(2015)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(2016)).to.be.true;
+		expect(adhan.Astronomical.isLeapYear(1600)).to.be.true;
+		expect(adhan.Astronomical.isLeapYear(2000)).to.be.true;
+		expect(adhan.Astronomical.isLeapYear(2400)).to.be.true;
+		expect(adhan.Astronomical.isLeapYear(1700)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(1800)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(1900)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(2100)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(2200)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(2300)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(2500)).to.be.false;
+		expect(adhan.Astronomical.isLeapYear(2600)).to.be.false;
+	});
+
+	it("gets the day of the year for a date", function() {
+		expect(dayOfYear(new Date(2015, 0, 1))).to.equal(1);
+		expect(dayOfYear(new Date(2015, 11, 31))).to.equal(365);
+		expect(dayOfYear(new Date(2016, 11, 31))).to.equal(366);
+		expect(dayOfYear(new Date(2015, 1, 1))).to.equal(32);
+	});
+
+	it("calculates the days since the winter or summer solstice", function() {
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 0, 1)), 2016, 1)).to.equal(11);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 11, 31)), 2015, 1)).to.equal(10);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 31)), 2016, 1)).to.equal(10);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 21)), 2016, 1)).to.equal(0);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 22)), 2016, 1)).to.equal(1);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 2, 1)), 2016, 1)).to.equal(71);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 2, 1)), 2015, 1)).to.equal(70);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 20)), 2016, 1)).to.equal(365);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 11, 20)), 2015, 1)).to.equal(364);
+
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 5, 21)), 2015, -1)).to.equal(0);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 5, 21)), 2016, -1)).to.equal(0);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 5, 20)), 2015, -1)).to.equal(364);
+		expect(adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 5, 20)), 2016, -1)).to.equal(365);
+	});
 });
-
-
-
-/*
-
-
-QUnit.test("Solar Time", function(assert) {
-    // Comparison values generated from http://aa.usno.navy.mil/rstt/onedaytable?form=1&ID=AA&year=2015&month=7&day=12&state=NC&place=raleigh
-    var coordinates = new adhan.Coordinates(35 + 47/60, -78 - 39/60);
-    var solar = new adhan.SolarTime(new Date(2015, 6, 12), coordinates);
-
-    var transit = solar.transit;
-    var sunrise = solar.sunrise;
-    var sunset = solar.sunset;
-    var twilightStart = solar.hourAngle(-6, false);
-    var twilightEnd = solar.hourAngle(-6, true);
-    var invalid = solar.hourAngle(-36, true);
-    assert.equal(timeString(twilightStart), "9:38");
-    assert.equal(timeString(sunrise), "10:08");
-    assert.equal(timeString(transit), "17:20");
-    assert.equal(timeString(sunset), "24:32");
-    assert.equal(timeString(twilightEnd), "25:02");
-    assert.equal(timeString(invalid), "");
-});
-
-QUnit.test("Right Ascension Edge Case", function(assert) {
-    var coordinates = new adhan.Coordinates(35 + 47/60, -78 - 39/60);
-    var solar = [];
-    for (var i = 0; i <= 365; i++) {
-        solar.push(new adhan.SolarTime(new Date(2016, 0, i), coordinates));
-    }
-
-    for (var i = 1; i < solar.length; i++) {
-        var time = solar[i];
-        var previousTime = solar[i-1];
-        assert.ok(Math.abs(time.transit - previousTime.transit) < 1/60);
-        assert.ok(Math.abs(time.sunrise - previousTime.sunrise) < 2/60);
-        assert.ok(Math.abs(time.sunset - previousTime.sunset) < 2/60);
-    }
-});
-
-QUnit.test("Calendrical Date", function(assert) {
-	// generated from http://aa.usno.navy.mil/data/docs/RS_OneYear.php for KUKUIHAELE, HAWAII
-    var coordinates = new adhan.Coordinates(20 + 7/60, -155 - 34/60);
-    var day1solar = new adhan.SolarTime(new Date(2015, 3, 2), coordinates);
-    var day2solar = new adhan.SolarTime(new Date(2015, 3, 3), coordinates);
-
-    var day1 = day1solar.sunrise;
-    var day2 = day2solar.sunrise;
-
-    assert.equal(timeString(day1), "16:15")
-    assert.equal(timeString(day2), "16:14")
-});
-
-QUnit.test("Interpolation", function(assert) {
-	var interpolatedValue = adhan.Astronomical.interpolate(0.877366, 0.884226, 0.870531, 4.35/24)
-	expect(interpolatedValue, 0.876125, 0.000001);
-
-    var i1 = adhan.Astronomical.interpolate(1, -1, 3, 0.6);
-    expect(i1, 2.2, 0.000001);
-});
-
-QUnit.test("Angle Interpolation", function(assert) {
-    var i1 = adhan.Astronomical.interpolateAngles(1, -1, 3, 0.6);
-    expect(i1, 2.2, 0.000001);
-
-    var i2 = adhan.Astronomical.interpolateAngles(1, 359, 3, 0.6);
-    expect(i2, 2.2, 0.000001);
-});
-
-QUnit.test("Julian Day", function(assert) {
-	// Comparison values generated from http://aa.usno.navy.mil/data/docs/JulianDate.php
-	assert.equal(adhan.Astronomical.julianDay(2010, 1, 2), 2455198.500000);
-	assert.equal(adhan.Astronomical.julianDay(2011, 2, 4), 2455596.500000);
-	assert.equal(adhan.Astronomical.julianDay(2012, 3, 6), 2455992.500000);
-	assert.equal(adhan.Astronomical.julianDay(2013, 4, 8), 2456390.500000);
-	assert.equal(adhan.Astronomical.julianDay(2014, 5, 10), 2456787.500000);
-	assert.equal(adhan.Astronomical.julianDay(2015, 6, 12), 2457185.500000);
-	assert.equal(adhan.Astronomical.julianDay(2016, 7, 14), 2457583.500000);
-	assert.equal(adhan.Astronomical.julianDay(2017, 8, 16), 2457981.500000);
-	assert.equal(adhan.Astronomical.julianDay(2018, 9, 18), 2458379.500000);
-	assert.equal(adhan.Astronomical.julianDay(2019, 10, 20), 2458776.500000);
-	assert.equal(adhan.Astronomical.julianDay(2020, 11, 22), 2459175.500000);
-	assert.equal(adhan.Astronomical.julianDay(2021, 12, 24), 2459572.500000);
-
-	var jdVal = 2457215.67708333;
-	expect(adhan.Astronomical.julianDay(2015, 7, 12, 4.25), jdVal, 0.000001);
-
-    var date = new Date(2015, 6, 12, 4, 15);
-    expect(julianDate(date), jdVal, 0.000001);
-
-	expect(adhan.Astronomical.julianDay(2015, 7, 12, 8.0), 2457215.833333, 0.000001);
-	expect(adhan.Astronomical.julianDay(1992, 10, 13, 0.0), 2448908.5, 0.000001);
-});
-
-QUnit.test("Julian Hours", function(assert) {
-	var j1 = adhan.Astronomical.julianDay(2010, 1, 3);
-	var j2 = adhan.Astronomical.julianDay(2010, 1, 1, 48);
-	assert.equal(j1, j2);
-});
-
-QUnit.test("Leap Year", function(assert) {
-	assert.notOk(adhan.Astronomical.isLeapYear(2015));
-	assert.ok(adhan.Astronomical.isLeapYear(2016));
-	assert.ok(adhan.Astronomical.isLeapYear(1600));
-	assert.ok(adhan.Astronomical.isLeapYear(2000));
-	assert.ok(adhan.Astronomical.isLeapYear(2400));
-	assert.notOk(adhan.Astronomical.isLeapYear(1700));
-	assert.notOk(adhan.Astronomical.isLeapYear(1800));
-	assert.notOk(adhan.Astronomical.isLeapYear(1900));
-	assert.notOk(adhan.Astronomical.isLeapYear(2100));
-	assert.notOk(adhan.Astronomical.isLeapYear(2200));
-	assert.notOk(adhan.Astronomical.isLeapYear(2300));
-	assert.notOk(adhan.Astronomical.isLeapYear(2500));
-	assert.notOk(adhan.Astronomical.isLeapYear(2600));
-});
-
-QUnit.test("Day of Year", function(assert) {
-	assert.equal(1, dayOfYear(new Date(2015, 0, 1)));
-	assert.equal(365, dayOfYear(new Date(2015, 11, 31)));
-	assert.equal(366, dayOfYear(new Date(2016, 11, 31)));
-	assert.equal(32, dayOfYear(new Date(2015, 1, 1)));
-});
-
-QUnit.test("Days since solstice", function(assert) {
-	assert.equal(11, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 0, 1)), 2016, 1));
-	assert.equal(10, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 11, 31)), 2015, 1));
-	assert.equal(10, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 31)), 2016, 1));
-	assert.equal(0, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 21)), 2016, 1));
-	assert.equal(1, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 22)), 2016, 1));
-	assert.equal(71, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 2, 1)), 2016, 1));
-	assert.equal(70, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 2, 1)), 2015, 1));
-	assert.equal(365, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 11, 20)), 2016, 1));
-	assert.equal(364, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 11, 20)), 2015, 1));
-
-	assert.equal(0, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 5, 21)), 2015, -1));
-	assert.equal(0, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 5, 21)), 2016, -1));
-	assert.equal(364, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2015, 5, 20)), 2015, -1));
-	assert.equal(365, adhan.Astronomical.daysSinceSolstice(dayOfYear(new Date(2016, 5, 20)), 2016, -1));
-});
-*/
