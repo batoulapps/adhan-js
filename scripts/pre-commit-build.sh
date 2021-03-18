31fe86a0b731846685b89eeb08efbaad80bfbf1b
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2021-03-18 14:15:23 -0400

    chore: update pre-commit hook
    Updated the pre-commit hook to perform a build and then prevent committing
    if there are unstaged changes to Adhan.js or Adhan.js.map afterwards.

--- scripts/pre-commit-build.sh
+++ scripts/pre-commit-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 npm run build
-git add Adhan.js
-git add Adhan.js.map
+
+(git diff --quiet Adhan.js Adhan.js.map)
+FILE_STATUS=$?
+
+if [[ $FILE_STATUS -ne 0 ]]; then
+    echo "Adhan.js doesn't contain the latest changes. Please run \"npm run build\" and add Adhan.js and Adhan.js.map to your commit."
+    exit 1
+fi
