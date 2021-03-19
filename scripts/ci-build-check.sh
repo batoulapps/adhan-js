ed2cb51e6b152afdc347a9a773662c829a6a247e
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2021-03-18 20:02:25 -0400

    chore: move build check to github action

--- scripts/ci-build-check.sh
+++ scripts/ci-build-check.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-npm run build
+set -euo pipefail
 
 (git diff --quiet Adhan.js Adhan.js.map)
-FILE_STATUS=$?
+DIFF_STATUS=$?
 
-if [[ $FILE_STATUS -ne 0 ]]; then
+if [[ $DIFF_STATUS -ne 0 ]]; then
     echo "Adhan.js doesn't contain the latest changes. Please run \"npm run build\" and add Adhan.js and Adhan.js.map to your commit."
     exit 1
 fi
