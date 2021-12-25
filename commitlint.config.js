ae662dbcb5fdf9c984c88164d3891d34341bbfb4
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2021-12-24 19:05:27 -0500

    ci(commitlint): ignore release commits
    Adds a rule to the commitlint config
    to ignore linting for release commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = { 
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => message.startsWith('chore(release)')],
   rules: {
     'footer-max-line-length': [0],
   }
